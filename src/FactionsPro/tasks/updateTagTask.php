b3fdb5f93a6c4e90c5ee72e2b6a6124fbb3eaec0
iZeaoGamer <jameshayward14@icloud.com>
2019-01-25 11:50:10 +0000

    Added the files.

---
+++ src/FactionsPro/tasks/updateTagTask.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace FactionsPro\tasks;
+use FactionsPro\FactionsMain;
+use pocketmine\Server;
+use pocketmine\scheduler\Task;
+use pocketmine\Player;
+
+class updateTagTask extends Task{
+	public function __construct(FactionsMain $plugin){
+		$this->plugin = $plugin;
+	}
+	public function onRun(int $currentTick){
+		foreach(Server::getInstance()->getOnlinePlayers() as $player){
+			if($player instanceof Player){
+			$player->setNameTagVisible();
+			$f = $this->plugin->getPlayerFaction($player->getName());
+				$name = $player->getName();
+			$player->setScoreTag(str_replace(["{player}", "{faction}"], [$player->getName(), $f], $this->plugin->prefs->get("faction-tag")));
+			}
+		}
+	}
+}
